2daaadb92583eca54695d5189ee6b0e0f336211f
nimblecrnch007 <suhasshrinivasan@users.noreply.github.com>
2014-07-12 17:38:13 +0530

    Create playwav.py

---
+++ playwav.py
@@ -0,0 +1,33 @@
+import pyaudio  # for python audio processing methods
+import wave # to handle .wav files
+import sys
+
+wavfile = wave.open(sys.argv[1], 'rb') # pass the .wav filename to be played as a command line argument
+p = pyaudio.PyAudio()
+
+CHUNK = 1024
+WIDTH = wavfile.getsampwidth()
+FORMAT = p.get_format_from_width(WIDTH)
+CHANNELS = wavfile.getnchannels()
+RATE = wavfile.getframerate()
+
+# open output stream
+outputStream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True)
+
+# read data
+data = wavfile.readframes(CHUNK)
+
+# play output stream
+while data != '':
+    outputStream.write(data)
+    data = wavfile.readframes(CHUNK)
+
+# stop and then close output stream
+stream.stop_stream()
+stream.close()
+
+# close file
+wavfile.close()
+
+# terminate PyAudio
+p.terminate()
