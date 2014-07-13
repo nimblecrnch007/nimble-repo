8cc0881270418958d905b3ea1ba6d38996126d70
Shreesha Aithal <9211shreesha@gmail.com>
2014-07-13 12:23:55 +0530

    Added features: (1) Lets the user decide the recording time and an option to discard the recording. (2) Saves the recording in a different file each time. (3) Saves the recording in a folder called "Saved recordings"
    Fixed a typo in playwav.py which wasn't letting the stream to be closed properly.

--- recordAndSaveAudio.py
+++ recordAndSaveAudio.py
@@ -1,16 +1,25 @@
 import pyaudio  # for python audio processing methods
 import wave # to handle .wav files
 import sys
+import os.path 
 
 FORMAT = pyaudio.paInt16	
 CHUNK = 1024	# frames_per_buffer
 CHANNELS = 2	# stereo
 RATE = 44100	# 44.1kHz sampling rate
-RECORD_SECONDS = 5	# 5 seconds of recording time
-OUTPUT_WAVEFILE = 'sampleaudio.wav'
+RECORD_SECONDS = int(raw_input("Enter the number of seconds to record: ")) 	
+#OUTPUT_WAVEFILE = 'sampleaudio.wav'
+TRACK_NO = 0	# Track number. As the name says. 
+OUTPUT_WAVEFILE = 'SampleAudio'+str(TRACK_NO)+'.wav'
+SAVE_FOLDER = 'Saved Recordings'
 
 if sys.platform == 'darwin':	# for Mac users
   CHANNELS = 1
+  
+if os.path.exists(SAVE_FOLDER):
+	pass
+else:
+	os.makedirs(SAVE_FOLDER)
 
 p = pyaudio.PyAudio()
 
@@ -32,10 +41,23 @@ inputStream.close()
 
 p.terminate()
 
-# now saving recorded audio onto a .wav file:
-wavfile = wave.open(OUTPUT_WAVEFILE, 'wb')
-wavfile.setnchannels(CHANNELS)
-wavfile.setsampwidth(p.get_sample_size(FORMAT))
-wavfile.setframerate(RATE)
-wavfile.writeframes(b''.join(frames))
-wavfile.close()
+save =  raw_input("Do you want to save the recording?(Press N to Discard): ")
+if save == ('N' or 'n'):
+	print "Discarding the record!"
+else:
+	# Checks if the file already exists in the folder.
+	SAVE_LOC = SAVE_FOLDER+'/'+OUTPUT_WAVEFILE
+	
+	while os.path.isfile(SAVE_LOC):
+		# generates a new file name for saving after confirming to save
+		TRACK_NO += 1
+		OUTPUT_WAVEFILE = 'SampleAudio'+str(TRACK_NO)+'.wav'
+		SAVE_LOC = SAVE_FOLDER+'/'+OUTPUT_WAVEFILE
+	
+	# now saving recorded audio onto a .wav file:
+	wavfile = wave.open(SAVE_LOC, 'wb')
+	wavfile.setnchannels(CHANNELS)
+	wavfile.setsampwidth(p.get_sample_size(FORMAT))
+	wavfile.setframerate(RATE)
+	wavfile.writeframes(b''.join(frames))
+	wavfile.close()
